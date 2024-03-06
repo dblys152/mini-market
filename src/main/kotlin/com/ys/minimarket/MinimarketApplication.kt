55662e3070487934109a125985ba3719d92ad1dc
youngseok <dblys152@gmail.com>
2024-03-06 22:25:55 +0900

    first commit

---
+++ src/main/kotlin/com/ys/minimarket/MinimarketApplication.kt
@@ -0,0 +1,11 @@
+package com.ys.minimarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MinimarketApplication
+
+fun main(args: Array<String>) {
+	runApplication<MinimarketApplication>(*args)
+}
---
+++ src/test/kotlin/com/ys/minimarket/MinimarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ys.minimarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MinimarketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
